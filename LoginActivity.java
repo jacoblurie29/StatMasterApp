03430f39e92935db7810de8f575ae59c8a6e69ff
Jacob Lurie <jacoblurie29@gmail.com>
2022-09-27 20:32:33 -0500

    Update LoginActivity.java

--- LoginActivity.java
+++ LoginActivity.java
@@ -44,72 +44,72 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 			
-				mEmailView = findViewById(R.id.Username_Email_FillIn);
-				mPasswordView = findViewById(R.id.Password_FillIn);
+	mEmailView = findViewById(R.id.Username_Email_FillIn);
+	mPasswordView = findViewById(R.id.Password_FillIn);
 
-				mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-						@Override
-						public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-								if (id == R.integer.login || id == EditorInfo.IME_NULL) {
-										attemptLogin();
-										return true;
-								}
-								return false;
-						}
-				});
+	mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+					if (id == R.integer.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+					}
+					return false;
+			}
+	});
 
-						// TODO: Grab an instance of FirebaseAuth
-						mAuth = FirebaseAuth.getInstance();
-				}
-
-				// Executed when Sign in button pressed
-				public void signInExistingUser(View v) {
-						// TODO: Call attemptLogin() here
-						attemptLogin();
-				}
-
-
-				// TODO: Complete the attemptLogin() method
-				private void attemptLogin() {
-
-				String email = mEmailView.getText().toString();
-				String password = mPasswordView.getText().toString();
-
-				if (email.equals("") || password.equals("")) return;
-
-				Toast.makeText(LoginActivity.this, "Login in progress...", Toast.LENGTH_SHORT).show();
-
-				// TODO: Use FirebaseAuth to sign in with email & password
-				mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-						@Override
-						public void onComplete(@NonNull Task<AuthResult> task) {
-								Log.d("StatMaster", "signInWithEmail() onComplete: " + task.isSuccessful());
-								SaveSharedPreference.setEmailAndPassWord(LoginActivity.this, mEmailView.getText().toString(), mPasswordView.getText().toString());
-								if (!task.isSuccessful()) {
-										Log.d("StatMaster", "Problem signing in: " + task.getException());
-										showErrorDialog("There was a problem signing in...");
-										Toast.makeText(LoginActivity.this, "Make sure you are logging in with your EMAIL not username!", Toast.LENGTH_SHORT).show();
-								} else {
-
-										Intent intent = new Intent(LoginActivity.this, UserHomeScreen.class);
-										finish();
-										startActivity(intent);
-								}
-						}
-				});
-
-
-		}
-
-		// TODO: Show error on screen with an alert dialog
-		private void showErrorDialog(String message) {
-				new AlertDialog.Builder(LoginActivity.this)
-								.setTitle("Oops")
-								.setMessage(message)
-								.setPositiveButton(android.R.string.ok, null)
-								.setIcon(android.R.drawable.ic_dialog_alert)
-								.show();
-		}
+			// TODO: Grab an instance of FirebaseAuth
+			mAuth = FirebaseAuth.getInstance();
 	}
+
+	// Executed when Sign in button pressed
+	public void signInExistingUser(View v) {
+			// TODO: Call attemptLogin() here
+			attemptLogin();
+	}
+
+
+	// TODO: Complete the attemptLogin() method
+	private void attemptLogin() {
+
+	String email = mEmailView.getText().toString();
+	String password = mPasswordView.getText().toString();
+
+	if (email.equals("") || password.equals("")) return;
+
+	Toast.makeText(LoginActivity.this, "Login in progress...", Toast.LENGTH_SHORT).show();
+
+	// TODO: Use FirebaseAuth to sign in with email & password
+	mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(@NonNull Task<AuthResult> task) {
+					Log.d("StatMaster", "signInWithEmail() onComplete: " + task.isSuccessful());
+					SaveSharedPreference.setEmailAndPassWord(LoginActivity.this, mEmailView.getText().toString(), mPasswordView.getText().toString());
+					if (!task.isSuccessful()) {
+							Log.d("StatMaster", "Problem signing in: " + task.getException());
+							showErrorDialog("There was a problem signing in...");
+							Toast.makeText(LoginActivity.this, "Make sure you are logging in with your EMAIL not username!", Toast.LENGTH_SHORT).show();
+					} else {
+
+							Intent intent = new Intent(LoginActivity.this, UserHomeScreen.class);
+							finish();
+							startActivity(intent);
+					}
+			}
+	});
+
+
+	}
+
+	// TODO: Show error on screen with an alert dialog
+	private void showErrorDialog(String message) {
+			new AlertDialog.Builder(LoginActivity.this)
+							.setTitle("Oops")
+							.setMessage(message)
+							.setPositiveButton(android.R.string.ok, null)
+							.setIcon(android.R.drawable.ic_dialog_alert)
+							.show();
+	}
+}
 
 
